707483d15ae7b3f1d17cdb103b26eb768acbbf2e
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-11-04 01:26:47 +0200

    fix r

--- Lab 3/compress0/psnr.py
+++ Lab 3/compress0/psnr.py
@@ -8,17 +8,9 @@ def mse(image1 : np.array, image2: np.array):
             dev_sum += dev.item() * dev.item()
     return dev_sum / (m * n)
 
-def max_dev(image1 : np.array, image2: np.array):
-    [n, m] = image1.shape
-    maxd = 0
-    for i in range(0, n):
-        for j in range(0, m):
-            dev = abs(image1[i, j] - image2[i, j])
-            maxd = max(dev, maxd)
-    return maxd
 
 def psnr(image1 : np.array, image2 : np.array):
-    r = max_dev(image1, image2)
+    r = 255
     _mse = mse(image1, image2)
     if _mse != 0:
         return 10 * np.log10(r*r/_mse)
